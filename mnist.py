d9a3323d637210053165651425b370651606dd78
Dario Cazzani <dcazzani@cisco.com>
2019-01-14 14:56:14 -0800

    Split feature model from classifier model and transform labels

--- mnist.py
+++ mnist.py
@@ -3,6 +3,8 @@ import torchvision
 import torchvision.datasets as datasets
 import torchvision.models as models
 from torch import nn, autograd as ag
+from torch.autograd import Variable
+
 import torch.nn.init as init
 
 import random
@@ -10,79 +12,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
+from models import *
+from sklearn import preprocessing
 
-
-seed = 0
+seed = 42
 random.seed(seed)
 rng = np.random.RandomState(seed)
 torch.manual_seed(seed)
 
 TOT_CLASSES = 10
-
-class Fire(nn.Module):
-
-    def __init__(self, inplanes, squeeze_planes,
-                 expand1x1_planes, expand3x3_planes):
-        super(Fire, self).__init__()
-        self.inplanes = inplanes
-        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
-        self.squeeze_activation = nn.ReLU(inplace=True)
-        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
-                                   kernel_size=1)
-        self.expand1x1_activation = nn.ReLU(inplace=True)
-        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
-                                   kernel_size=3, padding=1)
-        self.expand3x3_activation = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.squeeze_activation(self.squeeze(x))
-        return torch.cat([
-            self.expand1x1_activation(self.expand1x1(x)),
-            self.expand3x3_activation(self.expand3x3(x))
-        ], 1)
-
-class GraySqueezeNet(nn.Module):
-    def __init__(self, num_classes=1000):
-        super(GraySqueezeNet, self).__init__()
-        self.num_classes = num_classes
-
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, stride=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
-            Fire(64, 16, 64, 64),
-            Fire(128, 16, 64, 64),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
-            Fire(128, 32, 128, 128),
-            Fire(256, 32, 128, 128),
-            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
-            Fire(256, 48, 192, 192),
-            Fire(384, 48, 192, 192),
-            Fire(384, 64, 256, 256),
-            Fire(512, 64, 256, 256),
-        )
-        # Final convolution is initialized differently form the rest
-        final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.5),
-            final_conv,
-            nn.ReLU(inplace=True),
-            nn.AdaptiveAvgPool2d((1, 1))
-        )
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                if m is final_conv:
-                    init.normal_(m.weight, mean=0.0, std=0.01)
-                else:
-                    init.kaiming_uniform_(m.weight)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.classifier(x)
-        return x.view(x.size(0), self.num_classes)
 
 def shuffle_unison(a, b):
     c = list(zip(a, b))
@@ -108,14 +46,15 @@ class TaskGen(object):
         self.loader = torch.utils.data.DataLoader(self.trainset,
                                             batch_size=self.sample_data_size,
                                             shuffle=True)
+        self.label_encoder = preprocessing.LabelEncoder()
 
     def get_task(self):
         num_classes = random.randint(2, TOT_CLASSES)
+        # num_classes = 10
         selected_labels = random.sample(range(0, TOT_CLASSES), num_classes)
         _, (data, labels) = next(enumerate(self.loader))
         data = data.numpy()
         labels = labels.numpy()
-
 
         preprocessed_labels = []
         preprocessed_data = []
@@ -134,32 +73,51 @@ class TaskGen(object):
             preprocessed_data, preprocessed_labels = shuffle_unison(
                                         preprocessed_data, preprocessed_labels)
 
-        return np.asarray(preprocessed_data), np.asarray(preprocessed_labels)
+        # Cross entropy loss treats each label as an index, so retransform the
+        # labels to relative index given the number of classes
+        preprocessed_labels = np.asarray(preprocessed_labels)
+        self.label_encoder.fit(list(set(preprocessed_labels)))
+        preprocessed_labels = self.label_encoder.transform(preprocessed_labels)
+
+        return np.asarray(preprocessed_data), preprocessed_labels, num_classes
 
 if __name__ == '__main__':
     task_generator = TaskGen()
     device = torch.device('cpu')
-    model = GraySqueezeNet(num_classes=TOT_CLASSES)
+    model_f = GraySqueezeNetFeatures()
 
-    def train_on_batch(x, y):
+    # Keep different classifier models depending on the number of classes
+    classifiers_dictionary = {}
+
+    def train_on_batch(x, y, classifier):
         criterion = nn.CrossEntropyLoss()
 
         x = torch.tensor(x, dtype=torch.float, device=device)
         y = torch.tensor(y, dtype=torch.float, device=device)
-        model.zero_grad()
+        print(y)
+        model_f.zero_grad()
+        classifier.zero_grad()
 
-        outputs = model(x)
-        loss = criterion(outputs, y.long())
+        features = model_f(x)
+        outputs = classifier(features)
+        # print("output: {} - y: {}".format(outputs.shape, y.shape))
+        loss = criterion(outputs, Variable(y.long()))
         loss.backward()
         # optimizer.step()
-        for param in model.parameters():
+        for param in classifier.parameters():
+            param.data -= innerstepsize * param.grad.data
+        for param in model_f.parameters():
             param.data -= innerstepsize * param.grad.data
 
-    def predict(x):
+    def predict(x, f, c):
         x = torch.tensor(x, dtype=torch.float, device=device)
-        return model(x).data.numpy()
+        # features = model_f(x)
+        # outputs = model_c(features)
+        # return outputs.data.numpy()
+        features = f(x)
+        outputs = c(features)
+        return outputs.data.numpy()
 
-    plot = True
     innerstepsize = 0.02 # stepsize in inner SGD
     innerepochs = 1 # number of epochs of each inner SGD
     outerstepsize0 = 0.1 # stepsize of outer optimization, i.e., meta-optimization
@@ -167,22 +125,36 @@ if __name__ == '__main__':
 
     # Reptile training loop
     for iteration in range(niterations):
-        weights_before = deepcopy(model.state_dict())
         # Generate task
-        data, labels = task_generator.get_task()
+        data, labels, num_classes = task_generator.get_task()
+        print("Num classes: {}".format(num_classes))
+
+        if num_classes not in classifiers_dictionary.keys():
+            print("yo")
+            current_classifier = GraySqueezeNetClassifier(num_classes=num_classes)
+            classifiers_dictionary[num_classes] = current_classifier
+        else:
+            current_classifier = classifiers_dictionary[num_classes]
+
+        weights_f_before = deepcopy(model_f.state_dict())
+        weights_c_before = deepcopy(current_classifier.state_dict())
         for _ in range(innerepochs):
-            train_on_batch(data, labels)
+            train_on_batch(data, labels, current_classifier)
         # Interpolate between current weights and trained weights from this task
         # I.e. (weights_before - weights_after) is the meta-gradient
-        weights_after = model.state_dict()
+        weights_f_after = model_f.state_dict()
+        weights_c_after = current_classifier.state_dict()
         outerstepsize = outerstepsize0 * (1 - iteration / niterations) # linear schedule
-        model.load_state_dict({name :
-            weights_before[name] + (weights_after[name] - weights_before[name]) * outerstepsize
-            for name in weights_before})
+        model_f.load_state_dict({name :
+            weights_f_before[name] + (weights_f_after[name] - weights_f_before[name]) * outerstepsize
+            for name in weights_f_before})
+        current_classifier.load_state_dict({name :
+            weights_c_before[name] + (weights_c_after[name] - weights_c_before[name]) * outerstepsize
+            for name in weights_c_before})
         print("-----------------------------")
         print("iteration               {}".format(iteration+1))
 
-    print(predict(data[0][None, :, :, :]))
+    print(predict(data[0][None, :, :, :], model_f, current_classifier))
     print(labels)
     plt.imshow(data[0][0])
     plt.show()
---
+++ models.py
@@ -0,0 +1,138 @@
+import torch
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.models as models
+from torch import nn, autograd as ag
+import torch.nn.init as init
+seed = 0
+torch.manual_seed(seed)
+class Fire(nn.Module):
+
+    def __init__(self, inplanes, squeeze_planes,
+                 expand1x1_planes, expand3x3_planes):
+        super(Fire, self).__init__()
+        self.inplanes = inplanes
+        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU(inplace=True)
+        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes,
+                                   kernel_size=1)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes,
+                                   kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.squeeze_activation(self.squeeze(x))
+        return torch.cat([
+            self.expand1x1_activation(self.expand1x1(x)),
+            self.expand3x3_activation(self.expand3x3(x))
+        ], 1)
+
+class GraySqueezeNetFeatures(nn.Module):
+    def __init__(self):
+        super(GraySqueezeNetFeatures, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(64, 16, 64, 64),
+            Fire(128, 16, 64, 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(128, 32, 128, 128),
+            Fire(256, 32, 128, 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(256, 48, 192, 192),
+            Fire(384, 48, 192, 192),
+            Fire(384, 64, 256, 256),
+            Fire(512, 64, 256, 256),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_uniform_(m.weight)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        return x
+
+class GraySqueezeNetClassifier(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(GraySqueezeNetClassifier, self).__init__()
+        self.num_classes = num_classes
+
+        # Final convolution is initialized differently form the rest
+        final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            final_conv,
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if m is final_conv:
+                    init.normal_(m.weight, mean=0.0, std=0.01)
+
+    def forward(self, x):
+        x = self.classifier(x)
+        return x.view(x.size(0), self.num_classes)
+
+class GraySqueezeNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(GraySqueezeNet, self).__init__()
+        self.num_classes = num_classes
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(64, 16, 64, 64),
+            Fire(128, 16, 64, 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(128, 32, 128, 128),
+            Fire(256, 32, 128, 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            Fire(256, 48, 192, 192),
+            Fire(384, 48, 192, 192),
+            Fire(384, 64, 256, 256),
+            Fire(512, 64, 256, 256),
+        )
+        # Final convolution is initialized differently form the rest
+        final_conv = nn.Conv2d(512, self.num_classes, kernel_size=1)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            final_conv,
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                if m is final_conv:
+                    init.normal_(m.weight, mean=0.0, std=0.01)
+                else:
+                    init.kaiming_uniform_(m.weight)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x.view(x.size(0), self.num_classes)
+
+if __name__ == '__main__':
+    x = torch.randn(1, 1, 28, 28)
+    model_f = GraySqueezeNetFeatures()
+    model_c = GraySqueezeNetClassifier(10)
+
+    features = model_f(x)
+    outputs = model_c(features)
+    print(outputs)
+
+    model = GraySqueezeNet(10)
+    outputs = model(x)
+    print(outputs)
